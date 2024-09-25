92b0d95246619c957148fd2e475db8c914119925
Bioib <revandvoey@gmail.com>
2024-09-25 17:19:13 +0700

    refactor: Move `/tes/` to `/survey/`

---
+++ src/app/survey/page.jsx
@@ -0,0 +1,23 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import Survey from "@/components/Survey";
+import SignOut from "@/components/SignOut";
+
+export default function SurveyPage() {
+  const token = cookies().get("user-token");
+
+  if (!token || !token.value) return redirect("/");
+
+  return (
+    <div className="w-screen h-screen flex flex-col items-center text-black">
+      <div className="text-center">
+        <h1 className="text-lg xl:text-5xl font-bold mt-8">
+          Tes Jiwa Entrepeneur
+        </h1>
+        <SignOut />
+      </div>
+
+      <Survey />
+    </div>
+  );
+}
