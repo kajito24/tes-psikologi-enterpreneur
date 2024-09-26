46614d1ba9e677fa28f514d780ba404c9999d413
Bioib <revandvoey@gmail.com>
2024-09-26 15:23:13 +0700

    🔥 (next-config): Remove `experimental`

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    esmExternals: "loose",
-    serverComponentsExternalPackages: ["mongoose"],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
